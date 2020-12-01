e0366fd1f0ecd9bbaf1feaa91a2039b7b44c9730
Kohpai <whcwhc78@gmail.com>
2020-12-01 14:15:31 +0100

    :fire: remove unused dependency

--- KGSMRSTN/src/main/java/org/dice/kgsmrstn/selector/AbstractSummarizationSelectorHits.java
+++ KGSMRSTN/src/main/java/org/dice/kgsmrstn/selector/AbstractSummarizationSelectorHits.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.apache.jena.rdf.model.Model;
